ae0925441ec71d1070e62dd875249ec3d5235602
SODA1127 <dlrlwjd1127@naver.com>
2020-08-31 08:49:48 +0900

    App ID 추가 프로세스 확인

--- src/main/kotlin/io/soda1127/soldierletter/SoldierLetterApplication.kt
+++ src/main/kotlin/io/soda1127/soldierletter/SoldierLetterApplication.kt
@@ -1,7 +1,9 @@
 package io.soda1127.soldierletter
 
 import mu.KLogging
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.ApplicationPidFileWriter
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
 
@@ -12,5 +14,7 @@ class SoldierLetterApplication {
 }
 
 fun main(args: Array<String>) {
-    runApplication<SoldierLetterApplication>(*args)
+    val app = SpringApplication(SoldierLetterApplication::class.java)
+    app.addListeners(ApplicationPidFileWriter())   // pid 를 작성하는 역할을 하는 클래스 선언
+    app.run(*args)
 }
